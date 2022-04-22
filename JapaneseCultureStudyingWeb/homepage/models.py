f4bde5372a62678c65ab6b96fc448cae06b0a74c
sunsh1ne2k <tqluc01012000@gmail.com>
2022-04-22 20:52:22 +0700

    Backend|Model : Init project skeleton

---
+++ JapaneseCultureStudyingWeb/homepage/models.py
@@ -0,0 +1,89 @@
+from djongo import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, null=False,
+                            blank=False, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, null=False,
+                            blank=False, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+    
+class Publisher(models.Model):
+    name = models.CharField(max_length=255, null=False,
+                            blank=False, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Content(models.Model):
+    # Constant for Status
+    DRAFT = "DRAFT"
+    POSTED = "POSTED"
+    DELETED = "DELETED"
+    STATUS_CHOICE = (
+        (DRAFT, "Draft"),
+        (POSTED, "Posted"),
+        (DELETED, "Deleted"),
+    )
+    
+    # Constant for content types
+    CENSORED = "CENSORED"
+    UNCENSORED = "UNCENSORED"
+    TYPE_CHOICE = (
+        (CENSORED, "Censored"),
+        (UNCENSORED, "Uncensored"),
+    )
+
+    # Fields
+    slug = models.SlugField(max_length=255,
+                            unique=True, null=False,
+                            blank=False,)
+    
+    title = models.CharField(max_length=255)
+    thumbnail = models.ImageField(null=False, blank=False,)
+    content = models.FileField(blank=True, null=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    publisher = models.ManyToManyField(Publisher, blank=False, null=False, related_name="contents")
+    categories = models.ManyToManyField(Category,
+                                        blank=False, null=False,
+                                        related_name="categories")
+
+    publication_date = models.IntegerField(default="2022",
+                                           null=False,
+                                           blank=False,
+                                           validators=[MaxValueValidator(2022), MinValueValidator(1900)])
+
+    active = models.BooleanField(default=True)
+
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICE,
+                              null=True)
+    # Foreign Field
+    author = models.ManyToManyField(Author,
+                               related_name="contents")
+    publisher = models.ManyToManyField(Publisher,
+                               related_name="contents")
+    categories = models.ManyToManyField(Category)
+    # Embedded Field
+    # comments = models.EmbeddedField(model_container="Comment")
+
+    def __str__(self):
+        return f"{ self.author } | { self.title }"
+
+    # def get_absolute_url(self):
+    #     return reverse('content-detail-slug', kwargs={"slug", self.slug })
+
+
+class ContentSeries(models.Model):
+    name = models.CharField(max_length=255)
+    contents = models.ManyToManyField(Content, related_name="contents")
+
