48100a119d8dd8762397900e7622747cc0fbe7c0
sunsh1ne2k <tqluc01012000@gmail.com>
2022-04-23 02:01:41 +0700

    Backend| Endpoint: add new endpoint for retrieve, edit, delete Content, Author, Category, Publisher

--- homepage/api/urls.py
+++ homepage/api/urls.py
@@ -2,8 +2,32 @@ from django.urls import path
 from homepage.api.views import *
 
 urlpatterns = [
-    path('authors', AuthorListCreateAPIView.as_view(), name='author-list'),
+    path('authors', 
+         AuthorListCreateAPIView.as_view(), 
+         name='author-list'),
+    
+    path('authors/<int:pk>', 
+         AuthorListCreateAPIView.as_view(), 
+         name='author-detail'),
     path('categories', CategoryListCreateAPIView.as_view(), name='category-list'),
-    path('publishers', PublisherListCreateAPIView.as_view(), name='publisher-list'),
-    path('contents', ContentListCreateAPIView.as_view(), name='content-list'),
+    
+    path('categories/<int:pk>', 
+         CategoryDetailAPIView.as_view(),
+         name='category-detail'),
+
+    path('publishers', 
+         PublisherListCreateAPIView.as_view(), 
+         name='publisher-list'),
+    
+    path('publishers/<int:pk>', PublisherDetailAPIView.as_view(),
+         name='publisher-detail'),
+
+    path('contents', 
+         ContentListCreateAPIView.as_view(), 
+         name='content-list'),
+    
+    path('contents/<int:pk>', 
+         ContentDetailAPIView.as_view(), 
+         name='content-detail'),
+
 ]
